4e210067ebd39c725260ba045b3fa8f7168c319d
ObodoakorDavid <obodoakordavid@outlook.com>
2023-03-13 16:32:58 +0100

    made some changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,9 +4,12 @@ import React from "react";
 
 const Search = () => {
   return (
-    <form>
-      <input type="text" />
-    </form>
+    <div className=" container d-sm-flex justify-content-between">
+      <form>
+        <input type="text" />
+      </form>
+      <div>dropdown</div>
+    </div>
   );
 };
 
